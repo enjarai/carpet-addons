8e5a9a7a9f0c8bed4d5fe2092074f64b7eaf5cbf
whoImT <Timmi.Heeke@gmail.com>
2020-10-21 23:50:56 +0200

    Bump version to 1.0.4 + fix: client crash when mining piston head(#2))

--- src/main/java/carpetaddons/mixins/BlockMixin.java
+++ src/main/java/carpetaddons/mixins/BlockMixin.java
@@ -18,27 +18,31 @@ import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import static net.minecraft.block.Block.dropStack;
-import static net.minecraft.block.Block.getDroppedStacks;
+
+import static net.minecraft.block.Block.*;
 
 @Mixin(Block.class)
 public abstract class BlockMixin implements ItemConvertible {
 
+    @Shadow public abstract void afterBreak(World world, PlayerEntity player, BlockPos pos, BlockState state, BlockEntity blockEntity, ItemStack stack);
+
     /**
     * CarefulBreak
-    *   TODO: cleanup code and fix console spam
+    *   TODO: cleanup code
     * @author whoImT
      * @reason carefulBreak
     */
+
     @Overwrite
     public static void dropStacks(BlockState state, World world, BlockPos pos, BlockEntity blockEntity, Entity entity, ItemStack stack) {
         if (world instanceof ServerWorld) {
             if (CarpetAddonsSettings.carefulBreak && entity instanceof PlayerEntity && entity.isInSneakingPose()) {
-
                 getDroppedStacks(state, (ServerWorld) world, pos, blockEntity, entity, stack).forEach((itemStack) -> {
                     Item item = itemStack.getItem();
                     int itemAmount = itemStack.getCount();
@@ -55,20 +59,19 @@ public abstract class BlockMixin implements ItemConvertible {
             getDroppedStacks(state, (ServerWorld) world, pos, blockEntity, entity, stack).forEach((itemStack) -> {
                 dropStack(world, pos, itemStack);
             });
+            state.onStacksDropped((ServerWorld)world, pos, stack);
         }
-        state.onStacksDropped((ServerWorld)world, pos, stack);
     }
 
     //carefulBreak PISTON_HEADS
     @Inject(method = "onBreak", at = @At("HEAD"))
     private void onBreak1(World world, BlockPos pos, BlockState state, PlayerEntity player, CallbackInfo ci) {
-
         if (state.getBlock() == Blocks.PISTON_HEAD && CarpetAddonsSettings.carefulBreak && player.isInSneakingPose()) {
-            Direction direction = ((Direction) state.get(FacingBlock.FACING)).getOpposite();
+            Direction direction = state.get(FacingBlock.FACING).getOpposite();
             pos = pos.offset(direction);
             BlockState blockState = world.getBlockState(pos);
             Block block = world.getBlockState(pos).getBlock();
-            if (block == Blocks.PISTON || block == Blocks.STICKY_PISTON && (Boolean) blockState.get(PistonBlock.EXTENDED)) {
+            if (block == Blocks.PISTON || block == Blocks.STICKY_PISTON && blockState.get(PistonBlock.EXTENDED)) {
                 dropStacks(blockState, world, pos, null, player, player.getMainHandStack());
                 world.removeBlock(pos, false);
             }
