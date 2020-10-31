eba025baead812c3743b962b6faae86554e2bb1c
whoImT <Timmi.Heeke@gmail.com>
2020-10-31 17:32:51 +0100

    Revert "Merge branch '1.16.x' into 1.15.2"
    This reverts commit e08e9d3d2f20d066d521367a1669dd2b47ba2dcd.

--- src/main/java/carpetaddons/CarpetAddonsSettings.java
+++ src/main/java/carpetaddons/CarpetAddonsSettings.java
@@ -11,13 +11,6 @@ import static carpet.settings.RuleCategory.*;
 @SuppressWarnings("CanBeFinal")
 public class CarpetAddonsSettings
 {
-
-    @Rule(
-            desc = "Combines the duration of splash potions",
-            category = {SURVIVAL, EXPERIMENTAL, "carpetaddons"}
-    )
-    public static boolean stackingSplashPotions = false;
-
     @Rule(
             desc = "Backports 1.12 flint and steel behavior. Flint and steel can be used for updating observers / buds",
             category = {FEATURE, SURVIVAL, "carpetaddons"}
@@ -25,7 +18,7 @@ public class CarpetAddonsSettings
     public static boolean oldFlintAndSteelBehavior = false;
 
     @Rule(
-            desc = "Potion can be used instantly",
+            desc = "instant potions",
             category = {FEATURE, SURVIVAL, "carpetaddons"}
     )
     public static boolean instantPotions = false;
--- src/main/java/carpetaddons/mixins/FlintAndSteelItemMixin.java
+++ src/main/java/carpetaddons/mixins/FlintAndSteelItemMixin.java
@@ -1,7 +1,6 @@
 package carpetaddons.mixins;
 
 import carpetaddons.CarpetAddonsSettings;
-import net.minecraft.block.AbstractFireBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.item.FlintAndSteelItem;
 import net.minecraft.util.math.BlockPos;
@@ -10,10 +9,10 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(AbstractFireBlock.class)
-public class AbstractFireBlockMixin {
+@Mixin(FlintAndSteelItem.class)
+public class FlintAndSteelItemMixin {
 
-    @Redirect(method = "method_30032", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;canPlaceAt(Lnet/minecraft/world/WorldView;Lnet/minecraft/util/math/BlockPos;)Z"))
+    @Redirect(method = "canIgnite", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;canPlaceAt(Lnet/minecraft/world/WorldView;Lnet/minecraft/util/math/BlockPos;)Z"))
     private static boolean canPlaceAt(BlockState blockState, WorldView world, BlockPos pos){
         if(CarpetAddonsSettings.oldFlintAndSteelBehavior)
             return true;
